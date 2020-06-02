c5c7036aae16d6e80aee46a728f8713becf9db21
J3duardo <jesus_69eduardo@hotmail.com>
2020-06-01 21:43:24 -0400

    Footer y header

---
+++ footer.php
@@ -0,0 +1,41 @@
+<!-- Footer -->
+    <footer id="footer" class="dark">
+      <!-- Copyright -->
+      <div id="copyrights">
+        <div class="container clearfix">
+          <div class="col_half">
+            Copyright &copy; 2020 All Rights Reserved<br>
+            <div class="copyright-links">
+              <a href="#">Privacy Policy</a>
+            </div>
+          </div>
+          <div class="col_half col_last tright">
+            <div class="fright clearfix">
+              <a href="#" class="social-icon si-small si-borderless si-facebook">
+                <i class="icon-facebook"></i>
+                <i class="icon-facebook"></i>
+              </a>
+              <a href="#" class="social-icon si-small si-borderless si-twitter">
+                <i class="icon-twitter"></i>
+                <i class="icon-twitter"></i>
+              </a>
+            </div>
+            <div class="clear"></div>
+            <i class="icon-envelope2"></i> info@email.com <span class="middot">&middot;</span>
+            <i class="icon-headphones"></i> +55-5-5555-5555
+          </div>
+        </div>
+      </div>
+    </footer>
+
+    <!-- Botón go to top -->
+    <div id="gotoTop" class="icon-angle-up"></div>
+
+    <!-- JavaScript externo -->
+    <script src="js/jquery.js"></script>
+    <script src="js/plugins.js"></script>
+    <script src="js/functions.js"></script>
+
+    <?php wp_footer() ?>
+  </body>
+</html>
