ef1b3844a1b8e482e85672da9a46f16cc99fecf2
J3duardo <jesus_69eduardo@hotmail.com>
2020-06-03 08:00:05 -0400

    Functions: Generar nuevos settings en el customizer para redes sociales

--- functions.php
+++ functions.php
@@ -7,10 +7,12 @@
   include(get_theme_file_path("/includes/setup.php"));
   include(get_theme_file_path("/includes/custom-nav-walker.php"));
   include(get_theme_file_path("/includes/widgets.php"));
+  include(get_theme_file_path("/includes/theme-customizer.php"));
 
   // Hooks
   add_action("wp_enqueue_scripts", "my_theme_files");
   add_action("after_setup_theme", "setup_theme");
   add_action("widgets_init", "my_theme_widgets");
+  add_action("customize_register", "my_theme_customize_register");
 
   // Shortcodes
