d11650e0499b32456bd4699aa07574011dda21b2
J3duardo <jesus_69eduardo@hotmail.com>
2020-06-03 19:08:32 -0400

    Functions: Modificar el slider de posts populares desde el customizer

--- includes/theme-customizer-settings/customizer-settings.php
+++ includes/theme-customizer-settings/customizer-settings.php
@@ -69,24 +69,37 @@
   // Settings genéricos del theme customizer
   function misc_customizer_settings($wp_customize) {
     // Settings
+    // Mostrar/ocultar cuadro de búsqueda del header
     $wp_customize->add_setting("header_show_search", array(
       "default" => "Yes"
     ));
+    // Mostrar/ocultar shopping cart del header
     $wp_customize->add_setting("header_show_cart", array(
       "default" => "Yes"
     ));
+    // Modificar el texto copiryght del footer
     $wp_customize->add_setting("footer_copyright_text", array(
       "default" => "Copyright &copy; 2020 All Rights Reserved"
     ));
+    // Seleccionar url de la página term of services
     $wp_customize->add_setting("footer_tos_page", array(
       "default" => 0
     ));
+    // Seleccionar url de la página privacy policy en el footer
     $wp_customize->add_setting("footer_privacy_page", array(
       "default" => 0
     ));
+    // Mostrar/ocultar slider del header con los posts populares
+    $wp_customize->add_setting("show_header_popular_posts", array(
+      "default" => false
+    ));
+    // Modificar el título del slider del header con los posts populares
+    $wp_customize->add_setting("popular_posts_widget_title", array(
+      "default" => "Breaking News"
+    ));
 
 
-    // Sección
+    // Sección del customizer
     $wp_customize->add_section("misc_section", array(
       "title" => __("Theme Misc Settings", "my-custom-theme"),
       "priority" => 40,
@@ -94,7 +107,7 @@
     ));
 
 
-    // Controllers
+    // Controllers (campos, botones, etc, para modificar el contenido en el customizer)
     $wp_customize->add_control(new WP_Customize_Control($wp_customize, "header_show_search_input", array(
       "label" => __("Show Search Button in Header", "my-custom-theme"),
       "section" => "misc_section",
@@ -125,10 +138,19 @@
       "settings" => "footer_tos_page",
       "type" => "dropdown-pages"
     )));
-    $wp_customize->add_control(new WP_Customize_Control($wp_customize, "footer_set_privacy_page", array(
-      "label" => __("Footer Privacy Policy Page", "my-custom-theme"),
+    $wp_customize->add_control(new WP_Customize_Control($wp_customize, "header_popular_posts", array(
+      "label" => __("Header Popular Posts Widget", "my-custom-theme"),
       "section" => "misc_section",
-      "settings" => "footer_privacy_page",
-      "type" => "dropdown-pages"
+      "settings" => "show_header_popular_posts",
+      "type" => "checkbox",
+      "choices" => array(
+        "yes" => __("Yes", "my-custom-theme")
+      )
+    )));
+    $wp_customize->add_control(new WP_Customize_Control($wp_customize, "popular_posts_widget_title_input", array(
+      "label" => __("Popular Posts Widget Title", "my-custom-theme"),
+      "section" => "misc_section",
+      "settings" => "popular_posts_widget_title",
+      "type" => "text"
     )));
   }
